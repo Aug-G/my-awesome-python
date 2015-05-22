0e5a7f320c729837153a9ea75a7ddef885ee6437
August G <yuanyun1673@gmail.com>
2015-05-22 21:16:38 +0800

    google_hosts 加上系统判定

--- 0.tools/google_hosts.py
+++ 0.tools/google_hosts.py
@@ -4,21 +4,29 @@
 # 目前支持windows 持续支持mac
 
 import urllib
-import re
+import platform
+
 
 google_hosts_url = "http://www.360kb.com/kb/2_150.html"
 
 windows_hosts_path = "C:\\Windows\\System32\\drivers\\etc\\hosts"
+linux_hosts_path = "/etc/hosts"
 
 def get_html(url):
     page = urllib.urlopen(url)
     return page.read()
 
 if __name__ == "__main__":
-
+    os_name = platform.system()
     data = get_html(google_hosts_url)
     end_data = data[data.find("#google hosts"):data.rfind("#google hosts")].replace("&nbsp;", " ").replace("<br />","")
-    f = open(windows_hosts_path,"w")
+    path = ""
+    if os_name == "Windows":
+        path = windows_hosts_path
+    elif os_name == "Linux":
+        path = linux_hosts_path
+
+    f = open(path,"w")
     f.write(end_data)
     f.close()
 
